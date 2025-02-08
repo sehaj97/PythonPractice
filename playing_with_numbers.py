aa14059f05be6c6c0801bcbcbd12b37cf8d90f6d
Sehajpreet Singh <88715006+sehaj97@users.noreply.github.com>
2025-02-08 17:55:49 -0500

    Create playing_with_numbers.py

---
+++ playing_with_numbers.py
@@ -0,0 +1,13 @@
+
+for number in range(6):
+  print(number)
+
+numbers = list(range(7))
+print(numbers)
+
+
+list_of_squares=[number**2 for number in range(6)]
+print(list_of_squares)
+
+list_of_cubes=[number**3 for number in range(6)]
+print(list_of_cubes)
